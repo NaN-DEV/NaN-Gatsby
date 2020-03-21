fcb5e70bd29487f54fe2789cb5ec9a3333a67eaf
NaN <nan@MacBook-Air-NaN.local>
2020-03-21 16:29:07 +0100

    add plugin gatsby : SVG , Google font , eslint , Prettier , Layout

---
+++ src/layouts/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Root = () => (
+  <>
+    <p>
+      Lorem ipsumElitr dolor magna et elitr duo duo gubergren elitr ipsum, consetetur ipsum
+      consetetur labore ipsum at sea. Amet elitr sed nonumy sanctus labore ea, sit diam takimata
+      labore aliquyam lorem. Sit sed dolor eirmod nonumy dolores sadipscing eos ea, nonumy eos diam
+      accusam stet dolores dolores, dolores voluptua erat amet.
+    </p>
+  </>
+);
+
+export default Root;
