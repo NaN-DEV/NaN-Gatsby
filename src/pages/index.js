8a2a10245a8077a341af5279c98857ce256c4434
NaN <nan@MacBook-Air-NaN.local>
2020-06-26 19:12:49 +0200

    New

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,164 +1,22 @@
-// IMPORT PLUGIN
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react"
+import { Link } from "gatsby"
 
-// IMPORT COMPONENT
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
 
-import Section from '../components/organisms/section/section';
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
 
-// CREATE NEW COMPONENT
-const IndexPage = ({ data }) => {
-  const sectionOne = [
-    {
-      id: 'sectionHeroHome',
-      type: 'HeroFull',
-      color: 'primary',
-      arrow: 'triangleSection1',
-      textBold: 'Hello ',
-      textNormal: 'wolrd !',
-    },
-  ];
-
-  const sectionTwo = [
-    {
-      id: 'triangleSection1',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'FRONT END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection2',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection3',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection4',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection5',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection6',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection7',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection8',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection9',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection10',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection11',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-    {
-      id: 'triangleSection12',
-      type: 'Triangle',
-      title:
-        'Co to jest UX / UI ? Oraz czemu te zagadnienie jest aż tak ważne przy projektowaniu stron ?',
-      category: 'BACK END DEVELOPER',
-      image: data.file.childImageSharp.fixed,
-      link: '/box',
-    },
-  ];
-  const sectionTre = [
-    {
-      id: 'sellSection1',
-      type: 'Sell',
-      color: 'dark',
-      textBold: 'Hej! ',
-      textNormal: 'zróbmy coś razem ...',
-      title: 'Wyceń Twój pomysł',
-      link: '/sell',
-    },
-  ];
-  return (
-    <>
-      <Section content={sectionOne} />
-      <Section content={sectionTwo} />
-      <Section content={sectionTre} />
-    </>
-  );
-};
-
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "bitmap.png" }) {
-      childImageSharp {
-        fixed(width: 125, height: 125) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`;
-
-export default IndexPage;
+export default IndexPage
