8f8e9149f3766279af962593d1fbb9d5e67b59f9
NaN <nan@MacBook-Air-NaN.local>
2020-12-08 12:52:27 +0100

    update

--- src/components/organisms/section/sectionContact/sectionContact.js
+++ src/components/organisms/section/sectionContact/sectionContact.js
@@ -35,7 +35,7 @@ const sectionContactComponent = props => {
         <Row newClass="row">
           <FormBox theme={settings}>
             <Title>FORMULARZ</Title>
-            <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+            <form name="contact" method="POST" data-netlify="true">
               <p className="hidden">
                 <label>
                   Don’t fill this out if you’re human: <input name="bot-field" />
