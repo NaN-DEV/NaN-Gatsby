c468b1dbb6cc87e8c8d510dfe3cbf4e8c8f32a34
NaN <nan@MacBook-Air-NaN.local>
2020-12-08 11:55:54 +0100

    update

--- src/components/organisms/section/sectionContact/sectionContact.js
+++ src/components/organisms/section/sectionContact/sectionContact.js
@@ -35,19 +35,37 @@ const sectionContactComponent = props => {
         <Row newClass="row">
           <FormBox theme={settings}>
             <Title>FORMULARZ</Title>
-            <form name="Contact Form" method="POST" data-netlify="true">
-              <input type="hidden" name="form-name" value="Contact Form" />
-              <div>
-                <label>Your Email:</label>
-                <input type="email" name="email" />
-              </div>
-              <div>
-                <label>Message:</label>
-                <textarea name="message" />
-              </div>
-              <button type="submit" style={{ color: '#fff' }}>
-                Send
-              </button>
+            <form name="contact" method="POST" data-netlify="true">
+              <p>
+                <label>
+                  Your Name: <input type="text" name="name" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Email: <input type="email" name="email" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Role:
+                  <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                  </select>
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message: <textarea name="message" />
+                </label>
+              </p>
+              <p>
+                <input type="hidden" name="form-name" value="contact" />
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
             </form>
           </FormBox>
           <DataBox theme={settings}>
