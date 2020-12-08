260807aa9ed67a3249dff9c1233255c4647584aa
NaN <nan@MacBook-Air-NaN.local>
2020-12-08 13:24:21 +0100

    update

--- src/components/organisms/section/sectionContact/sectionContact.js
+++ src/components/organisms/section/sectionContact/sectionContact.js
@@ -46,22 +46,11 @@ const sectionContactComponent = props => {
               </p>
               <p>
                 <label>
-                  Your Role:
-                  <select name="role[]" multiple>
-                    <option value="leader">Leader</option>
-                    <option value="follower">Follower</option>
-                  </select>
-                </label>
-              </p>
-              <p>
-                <label>
                   Message: <textarea name="message" />
                 </label>
               </p>
               <p>
-                <button type="submit" style={{ color: 'white' }}>
-                  Send
-                </button>
+                <input type="submit" value="ok" />
               </p>
             </form>
           </FormBox>
